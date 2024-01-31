0181be903342bfaa91d7247ad164253bd05c172d
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2024-01-31 23:29:08 +0300

    Added manager files and basic method related to them.

--- iTikTok/Managers/HapticsManager.swift
+++ iTikTok/Managers/HapticsManager.swift
@@ -5,4 +5,27 @@
 //  Created by Ahmet Tarik DÖNER on 31.01.2024.
 //
 
-import Foundation
+import UIKit
+
+final class HapticsManager {
+    
+    static let shared = HapticsManager()
+    private init() {}
+    
+    //MARK: - Public
+    public func vibrateForSelection() {
+        DispatchQueue.main.async {
+            let generator = UISelectionFeedbackGenerator()
+            generator.prepare()
+            generator.selectionChanged()
+        }
+    }
+    
+    public func vibrate(for type: UINotificationFeedbackGenerator.FeedbackType) {
+        DispatchQueue.main.async {
+            let generator = UINotificationFeedbackGenerator()
+            generator.prepare()
+            generator.notificationOccurred(type)
+        }
+    }
+}
--- iTikTok/Managers/StorageManager.swift
+++ iTikTok/Managers/StorageManager.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import FirebaseStorage
+
+final class StorageManager {
+    
+    public static let shared = StorageManager()
+    private init() {}
+    
+    private let storage = Storage.storage().reference()
+    
+    //MARK: - Public
+    public func getVideoURL(
+        with identifier: String,
+        completion: (URL) -> Void
+    ) {
+        
+    }
+    
+    public func uploadVideoURL(from URL: String) {
+        
+    }
+}
