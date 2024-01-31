7ad23bb3027db6fb9eef5e6a9456a285c81f5d23
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2024-01-31 22:42:18 +0300

    Added Firebase and app icon.

--- iTikTok/Resources/AppDelegate.swift
+++ iTikTok/Resources/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,6 +21,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window.rootViewController = HomeViewController()
         window.makeKeyAndVisible()
         self.window = window
+        
+        FirebaseApp.configure()
         return true
     }
 }
